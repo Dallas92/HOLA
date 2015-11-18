e8b7bb8784ad544ecae7a81efeaa4eed615af91e
dallas92@outlook.com <55050392dallas92>
2015-11-18 18:58:32 +0300

    project structure

---
+++ HOLA/Views/Common/MainTabBar.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HOLA
+{
+	public partial class MainTabBar : ContentPage
+	{
+		public MainTabBar ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
---
+++ HOLA/Views/Common/SelectorPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HOLA
+{
+	public partial class SelectorPage : ContentPage
+	{
+		public SelectorPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
