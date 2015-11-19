6cf6f57a61e72fee4800fc89fd61be21acf15c2a
dallas92@outlook.com <55050392dallas92>
2015-11-19 20:34:17 +0300

    1

---
+++ HOLA/Views/Common/TabBarPage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using XLabs.Forms.Controls;
+
+namespace HOLA
+{
+	public class TabBarPage : ExtendedTabbedPage
+	{
+		public TabBarPage ()
+		{
+			this.Children.Add (new NavigationPage(new PlacesPage ()));
+			this.Children.Add (new NavigationPage(new PlacesSearchPage ()));
+			this.Children.Add (new NavigationPage(new PlacesNearbyPage ()));
+			this.Children.Add (new NavigationPage(new NewsPage ()));
+			this.Children.Add (new NavigationPage(new ProfilePage ()));
+
+			int x = 0;
+		}
+	}
+}
